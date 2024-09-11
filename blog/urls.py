5710c2b5703388c0ae3f80bca3575a5678ec2793
Maicon Souza <maicon_souza09@hotmail.com>
2024-09-11 19:16:55 -0300

    11-09-24 19:16

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home")
-]
+    path('', views.home, name="home"),
+    path('post/<int:post_id>/details', views.post_details, name='post_details')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils.text import Truncator
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    new_posts = [{'id': post.pk, 'autor': post.autor, 'titulo': post.titulo, 'resumo': Truncator(
+        post.conteudo).words(30, truncate='...')} for post in posts]
+
+    context = {
+        'posts': new_posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def post_details(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_details.html', context)
