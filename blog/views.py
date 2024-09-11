331288052a26b5cb5b3ed4a7a65091069ec330bc
Maixon Souza <maicon_souza09@hotmail.com>
2024-09-10 22:31:09 -0300

    10-09-24 22:31

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Boa Maicon!')
+    return render(request, 'blog/home.html')
